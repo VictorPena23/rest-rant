c0076496d4885cbd9850dece6f44ed14c9d86084
VictorPena23 <halo_rider200219@yahoo.com>
2023-03-04 17:47:36 -0800

    Adding and renderinga new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 
-// GET  /places
+// GET /places/new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+// GET /places
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -16,11 +21,7 @@ router.get('/', (req, res) => {
         pic: 'https://www.orlandocatcafe.com/wp-content/uploads/2022/07/orlando-cat-cafe-interior.jpg'
       }]
       
-      res.render('places/index', { places });
-
-
-
+    res.render('places/index', { places });
 })
 
 module.exports = router
-
