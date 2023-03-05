8d745950e10e564e68d41c6e60860efd12c9f855
VictorPena23 <halo_rider200219@yahoo.com>
2023-03-04 18:15:16 -0800

    form

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,33 @@
-const router = require('express').Router()
-
-// GET /places/new
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+const router = require('express').Router();
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'https://cdn.vox-cdn.com/thumbor/FrLmG_YlsWzjIpoi7gGKZkCcuIs=/0x0:3000x930/1200x900/filters:focal(1260x225:1740x705):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62456851/Noodle_Asia.0.0.0.0.0.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://www.orlandocatcafe.com/wp-content/uploads/2022/07/orlando-cat-cafe-interior.jpg'
-      }]
-      
-    res.render('places/index', { places });
-})
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'https://cdn.vox-cdn.com/thumbor/FrLmG_YlsWzjIpoi7gGKZkCcuIs=/0x0:3000x930/1200x900/filters:focal(1260x225:1740x705):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62456851/Noodle_Asia.0.0.0.0.0.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'https://www.orlandocatcafe.com/wp-content/uploads/2022/07/orlando-cat-cafe-interior.jpg'
+  }];
 
-module.exports = router
+  res.render('places/index', { places });
+});
+
+// GET /places/new
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+// POST /places
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('POST /places');
+});
+
+module.exports = router;
