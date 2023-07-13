bec72ab4f382f00c952f28bc14ffd3abe5a18ad8
VictorPena23 <halo_rider200219@yahoo.com>
2023-07-12 20:59:26 -0700

    updating places.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,113 +1,134 @@
-const router = require('express').Router();
-const placesModel = require('../models/places.js')
+const router = require("express").Router();
+const db = require("../models");
 
-// GET /places
-router.get('/', (req, res) => {
-  const placesArr = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'https://cdn.vox-cdn.com/thumbor/FrLmG_YlsWzjIpoi7gGKZkCcuIs=/0x0:3000x930/1200x900/filters:focal(1260x225:1740x705):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62456851/Noodle_Asia.0.0.0.0.0.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'https://www.orlandocatcafe.com/wp-content/uploads/2022/07/orlando-cat-cafe-interior.jpg'
-  }];
-  
-  res.render('places/index', { places: placesArr });
+router.get("/new", (req, res) => {
+  res.render("places/new");
 });
 
-// Show
-router.get('/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const place = placesModel[id];
-  if (isNaN(id) || !place) {
-    res.render('error404');
-  } else {
-    res.render('places/show', { place: place });
-  }
-});
-
-// GET /places/new
-router.get('/new', (req, res) => {
-  res.render('places/new');
-});
-
-// POST /places
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400';
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown';
+    req.body.pic = undefined;
   }
   if (!req.body.state) {
-    req.body.state = 'USA';
-  }
-  placesModel.push(req.body);
-  res.redirect('/places');
-});
-
-// Export router
-module.exports = router;
-
-
-// GET /places
-router.get('/', (req, res) => {
-  let placesArr = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'https://cdn.vox-cdn.com/thumbor/FrLmG_YlsWzjIpoi7gGKZkCcuIs=/0x0:3000x930/1200x900/filters:focal(1260x225:1740x705):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62456851/Noodle_Asia.0.0.0.0.0.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'https://www.orlandocatcafe.com/wp-content/uploads/2022/07/orlando-cat-cafe-interior.jpg'
-  }];
-
-  res.render('places/index', { places: placesArr });
-});
-
-//Show
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-// GET /places/new
-router.get('/new', (req, res) => {
-  res.render('places/new');
-});
-
-// POST /places
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.state = undefined;
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = undefined;
+  }
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error:";
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value},`;
+          message += `${err.errors[field].message}`;
+        }
+        console.log("Validation error message", message);
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
+    });
+});
+
+router.get("/", (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
+
+router.get("/:id/edit", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
+
+router.post("/:id/comment", (req, res) => {
+  console.log(req.body);
+  req.body.rant = req.body.rant ? req.body.rant : undefined;
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save();
+          res.redirect(`/places/${req.params.id}`);
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+router.put("/:id", (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = undefined;
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = undefined;
   }
-  placesModel.push(req.body)
-  res.redirect('/places')
-})
+  if (!req.body.city) {
+    req.body.city = undefined;
+  }
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
+
+router.delete("/:id/comment/:rantId", (req, res) => {
+  db.Comment.findByIdAndDelete(req.params.rantId)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("PLACE", err);
+      res.render("error404");
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(303).redirect("/places");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
 
 module.exports = router;
